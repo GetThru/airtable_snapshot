81130a189eeea6b79e7c38dc65702bd0393c2121
Milton Mazzarri <me@milmazz.uno>
2019-12-02 13:18:56 -0600

    Fix compilation issues (#3)

--- test/airtable_snapshot_test.exs
+++ test/airtable_snapshot_test.exs
@@ -11,38 +11,12 @@ defmodule AirtableSnapshotTest do
         bucket_name: @bucket_name,
         key: System.get_env("AIRTABLE_KEY"),
         table: System.get_env("AIRTABLE_TABLE"),
-        base: System.get_env("AIRTABLE_BASE"),
-        filter_records: fn records ->
-          Enum.filter(records, &Map.has_key?(&1["fields"], "Service Names"))
-        end,
-        process_records: fn records ->
-          Enum.map(records, fn %{"fields" => fields} ->
-            {
-              fields["Reference Name"],
-              %{
-                "service_names" =>
-                  String.split(fields["Service Names"], ",") |> Enum.map(&String.trim(&1)),
-                "district_abbreviation" => fields["District Abbreviation"],
-                "system" => fields["System"],
-                "api_key" => fields["API Key"],
-                "tag_ids" => fields["Tag Ids"],
-                "active" => fields["Active"],
-                "reference_name" => fields["Reference Name"],
-                "strategy" => fields["Strategy"],
-                "report_to" => fields["Send Report To"],
-                "timezone" => fields["Timezone"] |> String.downcase(),
-                "reseller" => fields["Reseller"],
-                "reseller_emails" => fields["Reseller Emails"]
-              }
-            }
-          end)
-          |> Enum.into(%{})
-        end
+        base: System.get_env("AIRTABLE_BASE")
       })
 
     assert is_map(resp)
 
-    first_key = Map.keys(resp) |> List.first()
+    first_key = resp |> Map.keys() |> List.first()
     assert is_map(resp[first_key])
   end
 
@@ -52,38 +26,12 @@ defmodule AirtableSnapshotTest do
         bucket_name: @bucket_name,
         key: "!!!! BAD KEY FORCE FAILURE !!!!",
         table: System.get_env("AIRTABLE_TABLE"),
-        base: System.get_env("AIRTABLE_BASE"),
-        filter_records: fn records ->
-          Enum.filter(records, &Map.has_key?(&1["fields"], "Service Names"))
-        end,
-        process_records: fn records ->
-          Enum.map(records, fn %{"fields" => fields} ->
-            {
-              fields["Reference Name"],
-              %{
-                "service_names" =>
-                  String.split(fields["Service Names"], ",") |> Enum.map(&String.trim(&1)),
-                "district_abbreviation" => fields["District Abbreviation"],
-                "system" => fields["System"],
-                "api_key" => fields["API Key"],
-                "tag_ids" => fields["Tag Ids"],
-                "active" => fields["Active"],
-                "reference_name" => fields["Reference Name"],
-                "strategy" => fields["Strategy"],
-                "report_to" => fields["Send Report To"],
-                "timezone" => fields["Timezone"] |> String.downcase(),
-                "reseller" => fields["Reseller"],
-                "reseller_emails" => fields["Reseller Emails"]
-              }
-            }
-          end)
-          |> Enum.into(%{})
-        end
+        base: System.get_env("AIRTABLE_BASE")
       })
 
     assert is_map(resp)
 
-    first_key = Map.keys(resp) |> List.first()
+    first_key = resp |> Map.keys() |> List.first()
     assert is_map(resp[first_key])
   end
 end
